5bd209c7b87b4aba53da018eb5c66f85342adbd5
Shahbaaz Siddiqui <shahbaazsidd@gmail.com>
2022-01-30 21:43:48 -0500

    Add basic player movement

---
+++ Kingdom Fall/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // For detecting objects in the Ground layer (assigned in inspector)
+    public LayerMask groundLayer;
+
+    // rigidbody for physics
+    private Rigidbody2D rb;
+
+    // Raycast to determine if the player is on the ground
+    private RaycastHit2D hit;
+    private float raycastLength = 0.5f;
+
+    // movement parameters
+    float moveSpeed = 300f;
+    float moveDirection = 0;
+    bool jump;
+    float jumpHeight = 5f;
+
+    // called at start of game
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // detects movement inputs (left, right, jump)
+        moveDirection = Input.GetAxisRaw("Horizontal");
+        jump = Input.GetButton("Jump");
+    }
+
+    private void FixedUpdate()
+    {
+        // moves the player left/right based on input
+        rb.velocity = new Vector2(moveDirection * moveSpeed * Time.deltaTime, rb.velocity.y);
+
+        // lets the player jump and makes sure the player is on the ground before jumping
+        if (jump && isGrounded())
+            rb.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+    }
+
+    bool isGrounded()
+    {
+        // casts a line downwards from the player's position to detect if it hits the ground
+        hit = Physics2D.Raycast(transform.position, Vector2.down, raycastLength, groundLayer);
+
+        if (hit.collider != null) // checks if something in ground layer is hit
+        {
+            return true;
+        }
+        return false;
+    }
+}
