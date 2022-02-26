fa3a19e7b0c446d1ea61225b0dccf5c85cd45bac
Shahbaaz Siddiqui <shahbaazsidd@gmail.com>
2022-02-25 22:53:47 -0500

    Add enemy struggle and fix UI

--- Kingdom Fall/Assets/Scripts/Enemy.cs
+++ Kingdom Fall/Assets/Scripts/Enemy.cs
@@ -1,21 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     // reference to player control script
     PlayerControl playerControl;
 
-    // cooldown ui stuff
-    public Image icon;
-    bool isCooldown = false;
-
     void Start()
     {
         playerControl = GetComponent<PlayerControl>();
-        icon.fillAmount = 0;
     }
 
     void Update()
@@ -26,19 +20,6 @@ public class Enemy : MonoBehaviour
             if (playerControl.isPossessed)
             {
                 playerControl.Eject();
-                isCooldown = true;
-                icon.fillAmount = 1;
-            }
-        }
-        // cooldown ui
-        if (isCooldown)
-        {
-            icon.fillAmount -= 1 / playerControl.possessCooldown * Time.deltaTime;
-
-            if (icon.fillAmount <= 0)
-            {
-                icon.fillAmount = 0;
-                isCooldown = false;
             }
         }
     }
