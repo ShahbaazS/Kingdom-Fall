caf4754c6926d4e79d5c3a241cd62ac67dfbb9e7
just-playing <20kn2@queensu.ca>
2022-02-25 23:52:10 -0500

    Added health bar UI.

--- Kingdom Fall/Assets/Scripts/Health.cs
+++ Kingdom Fall/Assets/Scripts/Health.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Health : MonoBehaviour
 {
 
-    public int health = 100;
+    public float health = 100;
+    public float MaxHealth = 100;
 
     public GameObject deathEffect;
+    public GameObject healthUI;
+    public Slider slider;
+    
+    void Start(){
+        health = MaxHealth;
+        slider.value = CalculateHealth();
+    }
+
+    void Update(){
+        slider.value = CalculateHealth();
+
+        if (health < MaxHealth){
+            healthUI.SetActive(true);
+        }
+    }
+
+    float CalculateHealth(){
+        return health / MaxHealth;
+    }
 
     public void TakeDamage (int damage){
         health -= damage;
