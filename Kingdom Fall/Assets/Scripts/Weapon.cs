b8eeae0d809da96368a0a11cac7dbb4fb782a4d2
just-playing <20kn2@queensu.ca>
2022-02-25 20:52:19 -0500

    Added simple ability.

--- Kingdom Fall/Assets/Scripts/Weapon.cs
+++ Kingdom Fall/Assets/Scripts/Weapon.cs
@@ -6,6 +6,8 @@ public class Weapon : MonoBehaviour{
     
     public Transform FirePoint;
     public GameObject BulletPrefab;
+    public GameObject BulletPrefab1;
+    public float FireRate = 2f;
     // Update is called once per frame
     void Update()
     {
@@ -13,9 +15,16 @@ public class Weapon : MonoBehaviour{
             Shoot();
         }
 
+        if (Input.GetButtonDown("Fire2")){
+            ability();
+        }
     }
 
     void Shoot (){
         Instantiate(BulletPrefab, FirePoint.position, FirePoint.rotation);
+    }
+
+    void ability (){
+        Instantiate(BulletPrefab1, FirePoint.position, FirePoint.rotation);
     }
 }
